8709bd19441704b55ff2f7ba9c217bb200433f54
lujain <63684917+lujainalkandari@users.noreply.github.com>
2020-06-16 23:03:51 +0300

    DONE

--- ios-hw-1.playground/Contents.swift
+++ ios-hw-1.playground/Contents.swift
@@ -5,9 +5,9 @@
  
  */
 
-let name = "Ahmad"
+let name = " Ahmad "
 
-name = "Salem"
+ let name1 = " Salem "
 
 
 var myWeight = 60
@@ -15,11 +15,11 @@ var FriendWeight = 80.5
 var height = 1.5
 
 
-var weightSum = myWeight + FriendWeight
+var weightSum =  "myWeight " + " FriendWeight "
 
 
 
-var bmi = myWeight * (height * height)
+var bmi  = (myWeight) =  Int(height + height)
 
 /*
 - - - -
@@ -38,33 +38,36 @@ var bmi = myWeight * (height * height)
  */
 
 //Person 1
-var firstName1 = ""
-var lastName1 = ""
-var email1 = ""
-var phoneNumber1 = ""
-var age1 = 0
-var country1 = ""
-var area1 = ""
-var password = ""
-var isKuwaiti = false
+var firstName1 = "LUJAIN"
+var lastName1 = "ALKANDARI"
+var email1 = "LUJAINalkandari@gmail.com"
+var phoneNumber1 = 99565366
+var age1 = 17
+var country1 = "kuwait"
+var area1 = "space"
+var password = "password"
+var isKuwaiti = true
 
 //Person 2
-
-
-
-
-
-
-
-
-
+var SecondName = "mona"
+var  lastName2 = "alrashidi"
+var email2 = "monalrashidi@hotmail.com"
+var phoneNumber2 = 96009310
+var age2 = 16
+var country2 = "kuwait"
+var area2 = "mars"
+var password1 = "hihihi"
+var iskuwati = true
 
 //Person 3
-
-
-
-
-
+var thirdName = "Trump"
+var lastName3 = "ew"
+var email3 = "trump@whitehouse.com"
+var phoneNumber3 = 1888666
+var age3 = 74
+var area3 = "jahanam wa b3s al ma9eer"
+var password3 = "byebyebye"
+var iskuwaiti = false
 
 
 
@@ -72,13 +75,45 @@ var isKuwaiti = false
 /*
 - - - -
 # المطلوب الثالث
-* قم بمقارنة معلومات كل شخص مع الشخص الاخر
+* قم بمقارنة معلومات كل شخص مع الشخص ال
  * قارن الاعمار اذا كانت متساوية ام لا
  * قارن منطقة السكن اذا كانت متساوية ام لا
  * قارن اذا عمر الاشخاص اكبر من ١٨
  * قارن اذا عمر الاشخاص اصغر من ١٨
  * قارن اذا عمر الاشخاص يساوي  ١٧
  * قارن اذا عمر الاشخاص لا يساوي  ١٧
-
+ 
  
  */
+
+thirdName == firstName1
+SecondName == firstName1
+SecondName == thirdName
+email1 == email2
+email3 == email1
+phoneNumber1  ==  phoneNumber2
+phoneNumber1 == phoneNumber3
+phoneNumber2 == phoneNumber3
+
+age1 > age2
+age1  == age3
+area1 == area2
+area1 == area2
+area3 == area1
+area3 == area2
+age3 > 18
+age3 < 18
+age1 > 18
+age1 < 18
+age2 > 18
+age2 < 18
+age1 != 17
+age2 != 17
+age3 != 17
+age1 == 17
+age2 == 17
+age3 == 17
+
+
+
+
